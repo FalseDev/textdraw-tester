642bc32d105e19681a5857a7624c9428d19b08ec
FalseDev <59121948+FalseDev@users.noreply.github.com>
2021-07-12 13:54:20 +0530

    Fix CI

--- scripts/ci_rename.py
+++ scripts/ci_rename.py
@@ -2,6 +2,8 @@ import glob
 import os
 import sys
 
+proj = "rust-multiline-textdraw"
+
 args = sys.argv[1:]
 
 if len(args) == 2:
@@ -10,10 +12,10 @@ if len(args) == 2:
 else:
     rust, mode, target = args
 
-filenames = glob.glob("./target/**/rocket-trial")
+filenames = glob.glob(f"./target/**/{proj}")
 if not os.path.isdir("builds"):
     os.mkdir("builds")
 
 for filename in filenames:
-    dest = "./builds/" + "-".join(["rocket-trial", rust, mode[2:], target])
+    dest = "./builds/" + "-".join([proj, rust, mode[2:], target])
     os.rename(filename, dest)
